3a52670dd08babd5184dfb23074cff7a5fdc87fc
Vilson <vilsonzh@buffalo.edu>
2024-08-03 16:45:17 -0400

    Added burn, deal, and almost finished hand checking functionality

--- src/client.py
+++ src/client.py
@@ -1,12 +1,54 @@
 import pygame
+import random
 
 width = 500
 height = 500
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 clientNumber = 0
+#Deck will be a dictionary of cards, where cards are randomly chosen
+#1, 2, 3, and 4 can be spades, hearts, diamonds, and clovers in whatever order we want
+#11 = J, 12 = Q, 13 = K, 14 = A
+deck = {1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        2: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        3: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        4: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]}
+
+def burn(deck):
+    suit = deck.get(random.randint(1, 4))
+    if len(suit)==0:
+        burn(deck)
+    suit.pop(random.randint(0, len(suit)-1))
+
+def deal(deck, hand):
+    suit = random.randint(1, 4)
+    hand[suit].append(deck.get(suit).pop(random.randint(0, len(deck[suit]))))
+#Add a card to a player's hand and pop it from the deck
+
+#Player must have a hand and money
+#When community cards are drawn, add to all players' hands
+
+def check(hand):
+    temphand = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0, 14:0}
+    for suit in range(hand):
+        arr = hand[suit]
+
+        for value in arr:
+            temphand[value] += 1
+    if 4 in temphand:
+        return "4 of a Kind"
+    if 3 in temphand and 2 in temphand:
+        return "Full House"
+
+    return "High Card"
+
+
 
 class Player ():
+    hand = {1: [],
+            2: [],
+            3: [],
+            4: []}
     def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
