8399112f7230e8ef7502c873dd553442c4eee521
Vilson <vilsonzh@buffalo.edu>
2024-08-09 12:42:07 -0400

    Almost done with some basic functions

--- src/client.py
+++ src/client.py
@@ -6,9 +6,9 @@ height = 500
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 clientNumber = 0
-#Deck will be a dictionary of cards, where cards are randomly chosen
-#1, 2, 3, and 4 can be spades, hearts, diamonds, and clovers in whatever order we want
-#11 = J, 12 = Q, 13 = K, 14 = A
+# Deck will be a dictionary of cards, where cards are randomly chosen
+# 1, 2, 3, and 4 can be spades, hearts, diamonds, and clovers in whatever order we want
+# 11 = J, 12 = Q, 13 = K, 14 = A
 deck = {1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
         2: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
         3: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
@@ -16,29 +16,39 @@ deck = {1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
 
 def burn(deck):
     suit = deck.get(random.randint(1, 4))
-    if len(suit)==0:
+    if len(suit) <= 0:
         burn(deck)
     suit.pop(random.randint(0, len(suit)-1))
 
 def deal(deck, hand):
     suit = random.randint(1, 4)
     hand[suit].append(deck.get(suit).pop(random.randint(0, len(deck[suit]))))
-#Add a card to a player's hand and pop it from the deck
+# Add a card to a player's hand and pop it from the deck
 
-#Player must have a hand and money
-#When community cards are drawn, add to all players' hands
+# Player must have a hand and money
+# When community cards are drawn, add to all players' hands
 
 def check(hand):
-    temphand = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0, 14:0}
+    temphand = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0}
+    Samesuit5 = False
     for suit in range(hand):
         arr = hand[suit]
-
+        if len(arr) >= 5:
+            Samesuit5 = True
         for value in arr:
             temphand[value] += 1
     if 4 in temphand:
         return "4 of a Kind"
-    if 3 in temphand and 2 in temphand:
+    if 3 in temphand and 2 in temphand: #This one is kind of inaccurate, update later
         return "Full House"
+    if Samesuit5:
+        return "Flush"
+    #Straight
+    if 3 in temphand:
+        return "3 of a Kind"
+    #2 pair
+    if 2 in temphand:
+        return "2 of a Kind"
 
     return "High Card"
 
